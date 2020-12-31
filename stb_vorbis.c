2f209e1ee029ef4822befc95ab8139cd24cc3c29
Sirennus <Benny_6566756@hotmail.com>
2020-12-31 16:24:37 +1100

    Handle comment list of zero length
    Stops the decoder from returning OOM because it can't malloc(0)
    Also prevents it from trying to free NULL

--- stb_vorbis.c
+++ stb_vorbis.c
@@ -3642,19 +3642,22 @@ static int start_decoder(vorb *f)
    f->vendor[len] = (char)'\0';
    //user comments
    f->comment_list_length = get32_packet(f);
-   f->comment_list = (char**)setup_malloc(f, sizeof(char*) * (f->comment_list_length));
-   if (f->comment_list == NULL)                     return error(f, VORBIS_outofmem);
+   if (f->comment_list_length > 0) {
+      f->comment_list = (char**)setup_malloc(f, sizeof(char*) * (f->comment_list_length));
+      if (f->comment_list == NULL)                 return error(f, VORBIS_outofmem);
 
-   for(i=0; i < f->comment_list_length; ++i) {
-      len = get32_packet(f);
-      f->comment_list[i] = (char*)setup_malloc(f, sizeof(char) * (len+1));
-      if (f->comment_list[i] == NULL)               return error(f, VORBIS_outofmem);
-
-      for(j=0; j < len; ++j) {
-         f->comment_list[i][j] = get8_packet(f);
+      for(i=0; i < f->comment_list_length; ++i) {
+         len = get32_packet(f);
+         f->comment_list[i] = (char*)setup_malloc(f, sizeof(char) * (len+1));
+         if (f->comment_list[i] == NULL)           return error(f, VORBIS_outofmem);
+         
+         for(j=0; j < len; ++j) {
+            f->comment_list[i][j] = get8_packet(f);
+         }
+         f->comment_list[i][len] = (char)'\0';
       }
-      f->comment_list[i][len] = (char)'\0';
    }
+   else f->comment_list = NULL;
 
    // framing_flag
    x = get8_packet(f);
@@ -4191,10 +4194,12 @@ static void vorbis_deinit(stb_vorbis *p)
    int i,j;
 
    setup_free(p, p->vendor);
-   for (i=0; i < p->comment_list_length; ++i) {
-      setup_free(p, p->comment_list[i]);
+   if (p->comment_list_length > 0) {
+      for (i=0; i < p->comment_list_length; ++i) {
+         setup_free(p, p->comment_list[i]);
+      }
+      setup_free(p, p->comment_list);
    }
-   setup_free(p, p->comment_list);
 
    if (p->residue_config) {
       for (i=0; i < p->residue_count; ++i) {
